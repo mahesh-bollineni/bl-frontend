258d04ccea694299d3d272479a518f0bf1117706
MAHESH-0023 <bollineni.mahesh2006@gmail.com>
2026-03-23 15:14:58 +0530

    Fix: Correct CSS path for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/App.css';
+import '../styles/App.css';
 
 const Navbar = ({ view, setView, searchMode, setSearchMode, setSearchTerm, onLogout }) => (
   <nav className="navbar">
